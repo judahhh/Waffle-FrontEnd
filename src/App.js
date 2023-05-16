37392c740512c312751f19ffef6f15367fba290b
judahhh <jdh99080516@gmail.com>
2023-05-17 02:00:54 +0900

    feat : 각 페이지 별 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import ChatPage from "./pages/chat/ChatPage";
 import ChatDetail from "./pages/chat/ChatDetail";
 import VideoPage from "./pages/videochat/VideoPage";
 import VideoPage2 from "./pages/videochat/VideoPage2";
+import MyProfilePage from "./pages/profile/MyProfilePage";
+import Group from "./pages/group/Group";
+import Room from "./pages/room/Room";
+import PlanEdit from "./pages/plan/PlanEdit";
+import BoardPage from "./pages/board/BoardPage";
+import BoardDetail from "./components/board/BoardDetail";
+
 function App() {
   return (
     <>
@@ -15,6 +22,29 @@ function App() {
           <Route path="/" element={<Home></Home>}></Route>
           <Route path="/login" element={<Login></Login>}></Route>
           <Route path="/register" element={<Register></Register>}></Route>
+          <Route
+            path="/planEditNDelete"
+            element={<PlanEdit></PlanEdit>}
+          ></Route>
+
+          <Route path="/group/:group_id" element={<Group></Group>}></Route>
+          <Route path="/room/:room_id" element={<Room></Room>}></Route>
+          <Route
+            path="/group/:group_id/board"
+            element={<BoardPage></BoardPage>}
+          ></Route>
+          <Route
+            path="/group/:group_id/board/:board_id"
+            element={<BoardDetail></BoardDetail>}
+          ></Route>
+          <Route
+            path="/room/:room_id/board"
+            element={<BoardPage></BoardPage>}
+          ></Route>
+          <Route
+            path="/room/:room_id/board/:board_id"
+            element={<BoardDetail></BoardDetail>}
+          ></Route>
           <Route path="/chat" element={<ChatPage></ChatPage>}></Route>
           <Route
             path="/chat/:dm_id"
@@ -27,6 +57,10 @@ function App() {
           <Route
             path="/openvidu2/:dm_id"
             element={<VideoPage2></VideoPage2>}
+          ></Route>
+          <Route
+            path="/myprofile"
+            element={<MyProfilePage></MyProfilePage>}
           ></Route>
         </Routes>
       </BrowserRouter>
