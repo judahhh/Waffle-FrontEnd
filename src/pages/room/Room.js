ffa863f4a8fcef83cb959cec0a7dfa252b706e22
judahhh <jdh99080516@gmail.com>
2023-05-24 23:15:19 +0900

    fix : 룸 페이지 렌더링 시 무한루프 도는 오류 해결

--- src/pages/room/Room.js
+++ src/pages/room/Room.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useLocation, useParams } from "react-router-dom";
 
 import { Box, Toolbar } from "@mui/material";
@@ -11,35 +11,41 @@ import { useTypeStore, useHeaderMenuStore } from "../../store/Store";
 const Room = () => {
   const { room_id } = useParams();
   const location = useLocation();
-  // const room_name = location.state.room_name;
-  const room_name = "룸";
-  // const rooms = location.state.rooms;
-  const rooms = [{ room_name: "룸1", room_id: 1 }];
-  // const group_id = location.state.group_id;
-  const group_id = 13;
-  // const groups = location.state.groups;
-  const groups = [
-    { group_name: "그룹1", group_id: 13 },
-    { group_name: "그룹2", group_id: 15 },
-  ];
+  const room_name = location.state.room_name;
+  // const room_name = "룸";
+  const rooms = location.state.rooms;
+  // const rooms = [{ room_name: "룸1", room_id: 1 }];
+  const group_id = location.state.group_id;
+  // const group_id = 13;
+  const groups = location.state.groups;
+  // const groups = [
+  //   { group_name: "그룹1", group_id: 13 },
+  //   { group_name: "그룹2", group_id: 15 },
+  // ];
   const { setTypeRoom } = useTypeStore();
-  setTypeRoom(room_id);
+  useEffect(() => {
+    setTypeRoom(room_id);
+    localStorage.setItem("type", "room");
+    localStorage.setItem("room_id", room_id);
+  }, []);
 
   return (
     <div>
-      <Header />
-      <SidebarAtRoom
-        room_name={room_name}
-        rooms={rooms}
-        group_id={group_id}
-        groups={groups}
-      />
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
-      >
-        <Toolbar />
-        <Calendar type="room" type_id={room_id} />
+      <Box sx={{ display: "flex" }}>
+        <Header />
+        <SidebarAtRoom
+          room_name={room_name}
+          rooms={rooms}
+          group_id={group_id}
+          groups={groups}
+        />
+        <Box
+          component="main"
+          sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
+        >
+          <Toolbar />
+          <Calendar type="room" type_id={room_id} />
+        </Box>
       </Box>
     </div>
   );
