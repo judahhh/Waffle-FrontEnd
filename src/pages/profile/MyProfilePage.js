aaf724acb6638e5fcadb856417e6c8d7fb69a6c1
judahhh <jdh99080516@gmail.com>
2023-05-24 23:16:15 +0900

    feat : 프로필 메뉴 클릭시 렌더링 할 프로필 페이지 구현

--- src/pages/profile/MyProfilePage.js
+++ src/pages/profile/MyProfilePage.js
@@ -1,7 +1,47 @@
 import React from "react";
+import styled from "styled-components";
+import { Box, Toolbar, Grid, Divider } from "@mui/material";
+
+import Header from "../../components/header/Header";
+import SideBarAtHome from "../../components/sidebar/SidebarAtHome";
+import MyData from "../../components/profile/MyData";
+import OngoingProject from "../../components/profile/OngoingProject";
+
+const StyleProfileWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const MyProfilePage = () => {
-  return <div>MyProfilePage</div>;
+  return (
+    <div>
+      <Box sx={{ display: "flex" }}>
+        <Header />
+        <SideBarAtHome />
+        <Box
+          component="main"
+          sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
+        >
+          <Toolbar />
+          <StyleProfileWrapper>
+            {" "}
+            <MyData />
+          </StyleProfileWrapper>
+
+          <Grid container alignItems="center">
+            <Divider
+              orientation="vertical"
+              flexItem
+              sx={{
+                borderColor: "DarkGrey",
+              }}
+            />
+          </Grid>
+        </Box>
+      </Box>
+    </div>
+  );
 };
 
 export default MyProfilePage;
