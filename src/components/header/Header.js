d28a9cb683d9dbd2ae625dadb98a9fffbc5abd1d
judahhh <jdh99080516@gmail.com>
2023-05-24 23:13:10 +0900

    feat : 홈과 그룹,룸에 따라 헤더 메뉴 다르게 분기 처리

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,14 +10,26 @@ import {
   useTypeStore,
 } from "../../store/Store";
 
+import DefaultImage from "../../assets/DefaultImage.png";
+
 export const MyToolbar = styled(Toolbar)`
   background-color: white;
+`;
+const StyleViewImg = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 70%;
+  overflow: hidden;
+  object-fit: cover;
+  cursor: pointer;
+  position: fixed;
+  top: 15px;
+  right: 15px;
 `;
 export const HeaderBtn = styled.button`
   margin: 0;
   padding: 20px;
   font-size: 20px;
-  font-weight: bold;
   border: none;
   background-color: white;
   /*color,font-size */
@@ -29,6 +41,10 @@ export const HeaderBtn = styled.button`
   ::after {
     background-color: rgba(245, 182, 108, 0.1);
   }
+  &.selected {
+    background-color: rgba(245, 182, 108, 0.3);
+  }
+  font-family: "Inter", sans-serif;
 `;
 export const MyTitle = styled.h1`
   margin: 0;
@@ -46,14 +62,24 @@ const Header = () => {
   const { storeGroups } = useGroupsStore();
   const { storeRooms } = useGroupsStore();
   const moveChat = () => {
+    setHeaderMenu("chat");
     navigate("/chat");
   };
   const movetoBoard = () => {
+    console.log(storeGroups);
     setHeaderMenu("board");
     console.log(headerMenu);
     navigate(`/${type}/${type_id}/board`, {
       state: { type_name: type_name, groups: storeGroups, rooms: storeRooms },
     });
+  };
+  const moveProfile = () => {
+    setHeaderMenu("profile");
+    navigate("/myprofile");
+  };
+  const movePlan = () => {
+    setHeaderMenu("plan");
+    navigate(`/`);
   };
   return (
     <div>
@@ -66,13 +92,25 @@ const Header = () => {
         <MyToolbar>
           {type === "home" ? (
             <>
-              <HeaderBtn id="일정" onClick={() => navigate(`/`)}>
+              <HeaderBtn
+                id="일정"
+                onClick={movePlan}
+                className={headerMenu === "plan" ? "selected" : ""}
+              >
                 일정
               </HeaderBtn>
-              <HeaderBtn id="프로필" onClick={() => navigate("/myprofile")}>
+              <HeaderBtn
+                id="프로필"
+                onClick={moveProfile}
+                className={headerMenu === "profile" ? "selected" : ""}
+              >
                 프로필
               </HeaderBtn>
-              <HeaderBtn id="채팅" onClick={moveChat}>
+              <HeaderBtn
+                id="채팅"
+                onClick={moveChat}
+                className={headerMenu === "chat" ? "selected" : ""}
+              >
                 채팅
               </HeaderBtn>
             </>
@@ -84,7 +122,7 @@ const Header = () => {
               >
                 일정
               </HeaderBtn>
-              <HeaderBtn id="게시판" onClick={() => movetoBoard()}>
+              <HeaderBtn id="게시판" onClick={movetoBoard}>
                 게시판
               </HeaderBtn>
               <HeaderBtn id="프로필" onClick={() => navigate("/myprofile")}>
@@ -95,6 +133,9 @@ const Header = () => {
               </HeaderBtn>
             </>
           )}
+          {/* <p style={{ textAlign: "right" }}> */}
+          <StyleViewImg src={DefaultImage} onClick={moveProfile} />
+          {/* </p> */}
         </MyToolbar>
       </AppBar>
     </div>
