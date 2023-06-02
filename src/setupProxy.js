922dd29d2b7cdf7d6c3f1cca85b3f26440fc9815
judahhh <jdh99080516@gmail.com>
2023-06-02 19:08:17 +0900

    chore : https에 따른 소켓 wss로 설정 변경

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,9 +2,9 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (app) => {
   app.use(
-    createProxyMiddleware("/chat-stomp", {
+    createProxyMiddleware("/api/chat-stomp", {
       target: process.env.REACT_APP_SERVER_URL,
-      ws: true,
+      wss: true,
     })
   );
 };
